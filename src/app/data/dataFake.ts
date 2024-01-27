3a7caad913f0f6bad42a4817b3abf0b1f67b6cb9
Junn-era <junnera@gmail.com>
2024-01-27 15:26:40 -0300

    Content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Pequi do Cerrado Goiano",
+    "description": "Pequis do Cerrado Goiano desaparecem",
+    "photoCover":"https://agro2.com.br/wp-content/uploads/2022/09/Goias-apresenta-crescimento-de-8-1-na-extracao-de-pequi-750x375.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Pequi do Cerrado Goiano",
+    "description": "Pequis do Cerrado Goiano desaparecem",
+    "photoCover": "https://agenciacoradenoticias.go.gov.br/wp-content/uploads/2021/06/Emater-realiza-evento-para-apresentar-pesquisas-ineditas-sobre-pragas-e-doencas-do-pequizeiro-1215x807.jpeg"
+  }
+]
